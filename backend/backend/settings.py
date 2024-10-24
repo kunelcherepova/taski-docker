9e382b9318f4d0f61fc39e7f3dfe0fa6f343b9cb
Черепова Кунел <kunel.selbikova.com>
2024-10-24 22:32:53 +1100

    Add workflow with PEP8 linting_1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['kcherpract.hopto.org', '89.169.175.79', 'localhost', '127.0.0.1' ]
+ALLOWED_HOSTS = ['kcherpract.hopto.org', '89.169.175.79', 'localhost', '127.0.0.1']
 
 # Application definition
 
